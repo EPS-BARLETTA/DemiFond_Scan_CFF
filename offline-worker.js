b740c42f224ec3221b6134120edb9ddc244d5ed5
EPS-BARLETTA <j.barletta@vauban.lu>
2026-09-13 08:37:26 +0200

    Active cache hors ligne de base

--- offline-worker.js
+++ offline-worker.js
@@ -1,3 +1,5 @@
-const VERSION='df-ccf-1';
-self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+const VERSION='df-ccf-2';
+const FILES=['/','/index.html','/app.css','/app.js','/manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(VERSION).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==VERSION).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(response=>{const copy=response.clone();caches.open(VERSION).then(cache=>cache.put(e.request,copy));return response}))) });
